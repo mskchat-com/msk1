95f5542d3ee3125a26f94db98ccff1a24e228287
jekr <48930765+a7mdal3le@users.noreply.github.com>
2023-04-06 02:08:43 +0300

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,4 @@
+$.getScript("https://raw.githack.com/mskchat-com/su3/main/2.js");
+$.getScript("https://raw.githack.com/mskchat-com/je2/main/kd.js");
+$.getScript("https://raw.githack.com/mskchat-com/j3/main/ed.js");
+$.getScript("https://raw.githack.com/mskchat-com/je2/main/kb3.js");
