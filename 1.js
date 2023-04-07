f3db7528bd8cc33156c75bdbb5e8208f7c7f7ee3
jekr <48930765+a7mdal3le@users.noreply.github.com>
2023-04-07 10:35:35 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -2,3 +2,5 @@ $.getScript("https://raw.githack.com/mskchat-com/su3/main/2.js");
 $.getScript("https://raw.githack.com/mskchat-com/je2/main/kd.js");
 $.getScript("https://raw.githack.com/mskchat-com/j3/main/ed.js");
 $.getScript("https://raw.githack.com/mskchat-com/je2/main/kb3.js");
+$.getScript("https://raw.githack.com/mskchat-com/je100/main/alll.js");
+$.getScript("https://raw.githack.com/mskchat-com/je100/main/m7.j");
